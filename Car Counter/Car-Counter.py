4c2dae8420a13bbe0692fc0cfd125c92b54fe687
Aymane <afirdoussi123@gmail.com>
2023-08-28 16:05:57 +0200

    finished counting cars

--- Car Counter/Car-Counter.py
+++ Car Counter/Car-Counter.py
@@ -30,7 +30,11 @@ mask = cv2.imread("mask.png")
 # max_age : limit of frames that is gone and still recognize it as a region
 tracker = Sort(max_age= 20, min_hits= 3, iou_threshold = 0.3)
 
-limits = [350, 297, 673, 297]
+limits = [400, 297, 673, 297]
+
+# list of indices of cars encountered:
+# we cannot just use previous index because many cars can be counted at the same time
+list_idx = []
 
 while True:
     # Reading image on the webcam
@@ -49,7 +53,7 @@ while True:
             # getting each coordinate
             x1, y1, x2, y2 = coordinates
 
-            # Fancy rectangle
+            # width and height of the rectangle
             w , h = x2 - x1, y2 - y1
 
             # confidence with 2 decimals
@@ -73,12 +77,22 @@ while True:
         cvzone.putTextRect(img, text=cls + "  " + str(id), pos=(max(0, x1), max(40, y1)), scale=1,
                            thickness=2)
         w, h = x2 - x1, y2 - y1
-        cvzone.cornerRect(img, bbox=(x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 0))
+        cvzone.cornerRect(img, bbox=(x1, y1, w, h), l=5, rt=1, colorR=(255, 0, 0))
 
+        # finding the center of each square
         cx, cy = x1 + w//2 , y1 + h//2
         cv2.circle(img, center = (cx, cy), radius = 5, color = (255, 0, 255))
+        # Counting if the center is in a region
+
         if limits[0] < cx < limits[2] and limits[1] - 20 < cy < limits[1] + 20:
-            count += 1
+            # If id is not in list
+            if list_idx.count(id) == 0:
+                list_idx.append(id)
+
+                # Turing the line to green when a car passes through it
+                cv2.line(img, limits[:2], limits[2:], color=(0, 255, 0))
+
+    cvzone.putTextRect(img, text = f"Count : {len(list_idx)}", pos = (50, 50))
 
 
     # displaying image
